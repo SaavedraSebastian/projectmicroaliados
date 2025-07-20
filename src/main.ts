d55b0a111997babfa25867b8fcb86fa087d5a4ef
Saavedra Sebastian <ssaavedraa4@upao.edu.pe>
2025-07-19 23:59:08 -0500

    Despliegue

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
+import { environment } from './environments/environment'; // 👈 importa esto primero
+
+// Protección básica contra DevTools
+if (environment.production) {
+  setInterval(() => {
+    const before = new Date().getTime();
+    debugger;
+    const after = new Date().getTime();
+    if (after - before > 100) {
+      window.location.reload();
+    }
+  }, 1000);
+}
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
-
   .catch((err) => console.error(err));
